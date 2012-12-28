3d6a8da96d266e7a2e0536f9173abafd9dd9bac5
Trevor Slocum <tslocum@gmail.com>
2012-12-28 00:10:16 -0800

    Polished settings descriptions

--- settings.default.php
+++ settings.default.php
@@ -1,20 +1,20 @@
 ﻿<?php
 define('TINYIB_BOARD', "b"); // Unique identifier for this board using only letters and numbers
-define('TINYIB_BOARDDESC', "TinyIB"); // Displayed in the logo area
-define('TINYIB_MAXTHREADS', 100); // Set this to limit the number of threads allowed before discarding older threads.  0 to disable
-define('TINYIB_TRUNCATE', 15); // Truncate messages to this many lines on board index pages.  0 to disable
-define('TINYIB_MAXKB', 2048); // Maximum file size.  0 to disable
-define('TINYIB_MAXKBDESC', "2 MB"); // Formatted maximum file size
-define('TINYIB_MAXW', 250); // Maximum image width.  Images exceeding this size will be thumbnailed
-define('TINYIB_MAXH', 250); // Maximum image height.  Images exceeding this size will be thumbnailed
-define('TINYIB_DELAY', 30); // Delay between posts to help control flooding.  0 to disable
+define('TINYIB_BOARDDESC', "TinyIB"); // Displayed at the top of every page
+define('TINYIB_MAXTHREADS', 100); // Oldest threads are discarded over this limit  [0 to disable]
+define('TINYIB_TRUNCATE', 15); // Messages are truncated to this many lines on board index pages  [0 to disable]
+define('TINYIB_MAXKB', 2048); // Maximum file size in kilobytes  [0 to disable]
+define('TINYIB_MAXKBDESC', "2 MB"); // Human-readable representation of the maximum file size
+define('TINYIB_MAXW', 250); // Maximum image width
+define('TINYIB_MAXH', 250); // Maximum image height - Images exceeding these sizes will be thumbnailed
+define('TINYIB_DELAY', 30); // Delay between posts to help control flooding  [0 to disable]
 define('TINYIB_LOGO', ""); // Logo HTML
-define('TINYIB_TRIPSEED', ""); // Text to use when generating secure tripcodes
+define('TINYIB_TRIPSEED', ""); // Enter some random text - Used when generating secure tripcodes - Must not change once set
 define('TINYIB_ADMINPASS', ""); // Text entered at the manage prompt to gain administrator access
-define('TINYIB_MODPASS', ""); // Same as above, but only has access to delete posts.  Blank ("") to disable
-define('TINYIB_DBMODE', "flatfile"); // flatfile / mysql / sqlite
+define('TINYIB_MODPASS', ""); // Moderators only have access to delete posts  ["" to disable]
+define('TINYIB_DBMODE', "flatfile"); // Choose: flatfile / mysql / sqlite
 
-// mysql settings - only edit if using mysql
+// Note: The following only apply when TINYIB_DBMODE is set to mysql
 define('TINYIB_DBHOST', "localhost");
 define('TINYIB_DBUSERNAME', "");
 define('TINYIB_DBPASSWORD', "");
