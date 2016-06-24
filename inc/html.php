f84085c07ea7d04e0d8f61be44a160b8c679b277
Trevor Slocum <tslocum@gmail.com>
2016-06-24 12:53:33 -0700

    Improve page display on mobile devices

--- inc/html.php
+++ inc/html.php
@@ -14,6 +14,7 @@ function pageHeader() {
 		<meta http-equiv="expires" content="0">
 		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT">
 		<meta http-equiv="pragma" content="no-cache">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
 		<title>
 EOF;
 	$return .= TINYIB_BOARDDESC . <<<EOF
