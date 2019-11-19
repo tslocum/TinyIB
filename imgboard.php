61303915feb8e5f833205722e91138d6224a2240
Trevor Slocum <trevor@rocketnine.space>
2019-11-18 18:26:36 -0800

    Check for magic quotes only when running running on PHP pre-5.3
    Fixes #92

--- imgboard.php
+++ imgboard.php
@@ -14,16 +14,18 @@ if (function_exists('ob_get_level')) {
 	}
 }
 
-if (get_magic_quotes_gpc()) {
-	foreach ($_GET as $key => $val) {
-		$_GET[$key] = stripslashes($val);
+if (version_compare(phpversion(), '5.3.0', '<')) {
+	if (get_magic_quotes_gpc()) {
+		foreach ($_GET as $key => $val) {
+			$_GET[$key] = stripslashes($val);
+		}
+		foreach ($_POST as $key => $val) {
+			$_POST[$key] = stripslashes($val);
+		}
 	}
-	foreach ($_POST as $key => $val) {
-		$_POST[$key] = stripslashes($val);
+	if (get_magic_quotes_runtime()) {
+		set_magic_quotes_runtime(0);
 	}
-}
-if (get_magic_quotes_runtime()) {
-	set_magic_quotes_runtime(0);
 }
 
 function fancyDie($message) {
